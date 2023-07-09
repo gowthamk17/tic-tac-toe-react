f8bbe6679af626446f8e47347af2733c1d03fb8b
gowthamk17 <kumargowtham.j@gmail.com>
2023-07-09 17:36:54 +0530

    game history added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,131 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
-  return <Square />
+  return <Game />
 }
 
+function Game() {
 
-function Square() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = (currentMove % 2 == 0);
+  const currentSquares = history[currentMove]
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (<li key={move} className='history-li'>
+      <button onClick={() => jumpTo(move)} className='history-btn'>{description}</button>
+    </li>);
+  })
+
+  function resetGame() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);  
+  }
+
+  return (
+    <div className='game'>
+      <button className='reset' onClick={resetGame}>Reset</button>
+      <div className='game-board'>
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol>
+          {moves}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+
+  // const [squares, setSquare] = useState(Array(9).fill(null))
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  }
+  else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const latestSquares = squares.slice();
+    if (xIsNext) {
+      latestSquares[i] = "X";
+    } else {
+      latestSquares[i] = "O";
+    }
+    onPlay(latestSquares);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
+      <h1>{status}</h1>
       <div className='board-row'>
-        <button className='square'>1</button>
-        <button className='square'>2</button>
-        <button className='square'>3</button>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className='board-row'>
-        <button className='square'>4</button>
-        <button className='square'>5</button>
-        <button className='square'>6</button>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className='board-row'>
-        <button className='square'>7</button>
-        <button className='square'>8</button>
-        <button className='square'>9</button>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
-} 
+}
+
+function Square({ value, onSquareClick }) {
+  return (<button className='square' onClick={onSquareClick}>{value}</button>);
+}
